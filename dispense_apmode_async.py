85504d9c3ce30d42096aef7c5ee3546efca865c8
Daniel Hirsch <daniel.hirsch@tngtech.com>
2022-02-02 00:48:14 +0100

    adds async dispense

---
+++ dispense_apmode_async.py
@@ -0,0 +1,84 @@
+#followed guide from https://randomnerdtutorials.com/esp32-esp8266-micropython-web-server/
+#this file in boot.py
+#get the micropython socket api
+try:
+  import usocket as socket
+except:
+  import socket
+
+#for connecting to the LED pin
+from machine import Pin
+#to connect to the wifi
+import network
+
+#this turns off the esp debug messages, not necessary!
+import esp
+esp.osdebug(None)
+
+#save flash memory by collecting unused memory
+import gc
+gc.collect()
+
+import ure
+import uasyncio as asyncio
+from micropython_switchbot import run_ble
+
+        
+#function to create the webpage
+def web_page(length):
+  html = """<html><head><title>Dispenser AP Mode</title> <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="data:,"> <style>html{font-family: Helvetica; display:inline-block; margin: 0px auto; text-align: center;}
+  h1{color: #0F3376; padding: 2vh;}p{font-size: 1.5rem;}
+  .button{display: inline-block; background-color: #e7bd3b; border: none; 
+  border-radius: 4px; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}
+  .button2{background-color: #4286f4;}
+  .button3{background-image: linear-gradient(#e7bd3b, #4286f4); }</style></head><body> <h1>Dispenser AP Mode</h1> 
+  <h2>Dispense Configuration</h2>
+  <form enctype="multipart/form-data" method="get">
+    <label>Dispense length in s</label><br/>
+    <input type="number" id="length" name="length" step="any" value='""" + str(length) + """'><br><br>
+    <input class="button" type="submit">
+  </form>
+  </body></html>"""
+  return html
+
+#create the webserver from the esp that the client connects to
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#bind the web server to a port, 80 for default
+s.bind(('', 80))
+#accept a maximum of 5 connections
+s.listen(5)
+s.setblocking(False)
+async def ap_mode(callback):
+    print("in ap mode")
+    length = 0
+    while True:
+      try:
+          print("on")
+          conn, addr = s.accept()
+          print('Got a connection from %s' % str(addr))
+          request = conn.recv(1024)
+          request = str(request)
+          print('Content = %s' % request)
+          request = str(request)
+          length = ure.search(r"/?length=(.*?) HTTP", request)
+          if length != None:
+            length = length.group(1)
+            on = b'\x57\x01\x01'
+            try:
+                asyncio.run(run_ble(on,int(length)*1000, callback))
+            except:
+                print("no number, try again")
+          response = web_page(length)
+          conn.send('HTTP/1.1 200 OK\n')
+          conn.send('Content-Type: text/html\n')
+          conn.send('Connection: close\n\n')
+          conn.sendall(response)
+          conn.close()
+      except OSError:
+          """If socket not set"""
+          await asyncio.sleep_ms(500)
+
+    
+
+
--- rot_test_withmodesasync.py
+++ rot_test_withmodesasync.py
@@ -5,10 +5,13 @@ import wot_client.api as apiX
 import sys
 from dispense_apmode import ap_mode
 from micropython_switchbot import run_on_api
+from light import init_lights
 import uasyncio as asyncio
 
 sys.path.reverse()
 apiX.init()
+init_lights()
+
 class Action:
     def __init__(self, mode):
         self.set_mode(mode)
@@ -17,18 +20,6 @@ class Action:
         self.mode = new_mode
         print("mode: ", self.mode)
         apiX.set_access_mode(new_mode) 
-
-    def run_dispenser(self, duration):
-        if self.mode == 1:
-            #create ap and control
-            ap_mode(apiX.publish_event)
-        elif self.mode == 2:
-            #create ap and control
-            ap_mode(apiX.publish_event)
-        elif self.mode == 3:
-            #internet
-            print("in 3")
-            #run_on_api(duration, apiX.publish_event)
         
 default_mode = 3
 a = Action(default_mode)
@@ -45,23 +36,16 @@ def init():
     return r, val_old
 
 
-apiX.set_access_mode(default_mode)
 def example_on_action_handler(topic, msg):
-    print('I am the example on_action_handler!', topic, msg)
-    # set msg to duration not mode!!! 
     run_on_api(int(msg), apiX.publish_event)
 
 apiX.set_on_action_handler(example_on_action_handler)
 
-print("done with set on")
-print("done with first part")
 r, val_old = init()
 
 val_old = r.value()
-event = asyncio.Event()
-
-async def rotated():
-    print("rotated")
+def rotated():
+    """check if rotary value changed"""
     global val_old
     val_new = math.floor(r.value()/5) 
     if val_old != val_new:
@@ -69,55 +53,35 @@ async def rotated():
         print('result =', val_new)
         apiX.publish_event("mode","test"+str(val_new))
         a.set_mode(val_new)
-        
-def callback():
-    print("callback")
-    event.set()
+        return True
+    return False
+
+async def check_modes(task):
+    """Kill ap mode if value changed"""
+    if rotated():
+        task.cancel()  
+    await asyncio.sleep_ms(1_000)
+
+async def main():
+    """
+    Start tasks in parallel
+    """
+    task = None
+    if a.mode == 1 or a.mode == 2:
+        task = asyncio.create_task(ap_mode(apiX.publish_event))
+    asyncio.create_task(check_modes(task))
+    await asyncio.sleep_ms(10_000)
     
 def rotary_send():
     while True:
         print("ready to publish event")
-        if val_old == 1 or val_old == 2:
-            ap_mode(apiX.publish_event)
-        time.sleep_ms(5000)
+        r.add_listener(rotated) # necessary?
+        asyncio.run(main())
+        time.sleep_ms(5000) 
 
-async def heartbeat():
-    print("heartbeat")
-    while True:
-        await asyncio.sleep_ms(10)
-        
-async def main():
-    r = RotaryIRQ(pin_num_clk=27, 
-                  pin_num_dt=26, 
-                  min_val=0, 
-                  max_val=19, 
-                  reverse=False, 
-                  range_mode=RotaryIRQ.RANGE_WRAP)
-                  
-    r.add_listener(callback)
-    
-    asyncio.create_task(heartbeat())
-    while True:
-        await event.wait()
-        global val_old
-        val_new = math.floor(r.value()/5) 
-        if val_old != val_new:
-            val_old = val_new
-            print('result =', val_new)
-            apiX.publish_event("mode","test"+str(val_new))
-            a.set_mode(val_new)
-            if val_old == 1 or val_old == 2:
-                print("going in")
-                await ap_mode(apiX.publish_event)
-                print("going out")
-        print('result =', math.floor(r.value()/5) )
-        event.clear()
-try:
-    asyncio.run(main())
-except (KeyboardInterrupt, Exception) as e:
-    print('Exception {} {}\n'.format(type(e).__name__, e))
-finally:
-    ret = asyncio.new_event_loop()  
+
+rotary_send()
+
 
 
 
